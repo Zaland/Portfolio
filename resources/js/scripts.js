bf261af605ae8310d877974b9c0f827ec1aaa23c
Unknown <zalandkhan1@gmail.com>
2017-10-09 15:58:12 -0300

    Added navbar with animations

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
-    /* Animations on scroll */
+    // Animations for projects section
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated bounceInUp');
-    }, {
-        offset: "40%",
-    });
+    }, { offset: "40%" });
     
-    /* Animated button */
-    $('.scroll-down-icon').click(function() {
+    // Animated scroll for nav buttons
+    $('.nav-home').click(function() {
+        $('html, body').animate({scrollTop: $('.js--main-nav').offset().top}, 1000); 
+    });
+
+    $('.nav-projects').click(function() {
         $('html, body').animate({scrollTop: $('.js--wp-1').offset().top}, 1000); 
     });
 
-    /* Run when contact form button is pressed */
+    $('.nav-contact').click(function() {
+        $('html, body').animate({scrollTop: $('.js--wp-2').offset().top}, 1000); 
+    });
+
+    // Run when contact form button is pressed
     $('.contact-form-submit').click(function(e) {
         e.preventDefault();
 
@@ -28,4 +34,51 @@ $(document).ready(function() {
             }
         });
     });
+
+    // Load sticky nav when reached a certain point
+    $('.js--home-nav').waypoint(function(direction) {
+        if(direction == "down") {
+            $('nav').addClass('animated fadeInDown');
+            $('nav').addClass('sticky-header-nav');
+        }
+        else {
+            $('nav').removeClass('animated fadeInDown');
+            $('nav').removeClass('sticky-header-nav');
+        }
+    }, { offset: '40%;' });
+    
+    $('.js--wp-1').waypoint(function(direction) {
+        if(direction == "down") {
+            $('nav').removeClass('sticky-header-nav');
+            $('nav').addClass('sticky');
+        }
+        else {
+            $('nav').removeClass('sticky');
+            $('nav').addClass('sticky-header-nav');
+        }
+    }, { offset: '5%;' });
+    
+    // Set the home nav link to highlight in the beginning
+    $('.nav-home').addClass('highlight');
+    
+    // If the waypoint for the home page is reached, set up the highlight
+    $('.js--home-nav').waypoint(function() {
+        $('.nav-home').addClass('highlight');
+        $('.nav-projects').removeClass('highlight');
+        $('.nav-contact').removeClass('highlight');
+    });
+    
+    // If the projects section is reached, set up the highlight
+    $('.js--wp-1').waypoint(function() {
+        $('.nav-home').removeClass('highlight');
+        $('.nav-projects').addClass('highlight');
+        $('.nav-contact').removeClass('highlight');
+    }, { offset: '5%;' });
+    
+    // If the contact me section is reached, set up the highlight
+    $('.js--wp-2').waypoint(function() {
+        $('.nav-home').removeClass('highlight');
+        $('.nav-projects').removeClass('highlight');
+        $('.nav-contact').addClass('highlight');
+    }, { offset: '70%;' });
 });
